2fb714b987de0ae0a0df01128019a7e27e6548f5
yonggyo1125 <yonggyo00@kakao.com>
2023-11-27 14:27:31 +0900

    수업소스

--- src/main/java/org/koreait/models/board/config/BoardConfigInfoService.java
+++ src/main/java/org/koreait/models/board/config/BoardConfigInfoService.java
@@ -1,7 +1,24 @@
 package org.koreait.models.board.config;
 
+import lombok.RequiredArgsConstructor;
+import org.koreait.commons.ListData;
+import org.koreait.entities.Board;
+import org.koreait.repositories.BoardRepository;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class BoardConfigInfoService {
+
+    private final BoardRepository repository;
+
+    public Board get(String bId) {
+        Board data = repository.findById(bId).orElseThrow(BoardNotFoundException::new);
+
+        return data;
+    }
+
+    public ListData<Board> getList(BoardSearch search) {
+
+    }
 }
