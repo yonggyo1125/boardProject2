79d34a5cc5f78daa149538a2379ebd9db61c937e
yonggyo1125 <yonggyo00@kakao.com>
2023-11-27 15:09:26 +0900

    수업소스

--- src/main/java/org/koreait/models/board/config/BoardConfigInfoService.java
+++ src/main/java/org/koreait/models/board/config/BoardConfigInfoService.java
@@ -1,8 +1,10 @@
 package org.koreait.models.board.config;
 
 import com.querydsl.core.BooleanBuilder;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.koreait.commons.ListData;
+import org.koreait.commons.Pagination;
 import org.koreait.commons.Utils;
 import org.koreait.controllers.admins.BoardSearch;
 import org.koreait.entities.Board;
@@ -20,6 +22,7 @@ import static org.springframework.data.domain.Sort.Order.desc;
 public class BoardConfigInfoService {
 
     private final BoardRepository repository;
+    private final HttpServletRequest request;
 
     public Board get(String bId) {
         Board data = repository.findById(bId).orElseThrow(BoardNotFoundException::new);
@@ -39,5 +42,13 @@ public class BoardConfigInfoService {
 
         Page<Board> data = repository.findAll(andBuilder, pageable);
 
+
+        Pagination pagination = new Pagination(page, (int)data.getTotalElements(), 10, limit, request);
+
+        ListData<Board> listData = new ListData<>();
+        listData.setContent(data.getContent());
+        listData.setPagination(pagination);
+
+        return listData;
     }
 }
