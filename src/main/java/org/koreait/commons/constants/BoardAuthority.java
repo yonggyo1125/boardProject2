2d1f613c9d634901211d301d9a37790feb05b1f6
yonggyo1125 <yonggyo00@kakao.com>
2023-11-27 16:24:53 +0900

    수업소스

--- src/main/java/org/koreait/commons/constants/BoardAuthority.java
+++ src/main/java/org/koreait/commons/constants/BoardAuthority.java
@@ -1,7 +1,28 @@
 package org.koreait.commons.constants;
 
+import java.util.Arrays;
+import java.util.List;
+
 public enum BoardAuthority {
-    ALL,
-    MEMBER,
-    ADMIN
+    ALL("비회원+회원+관리자"),
+    MEMBER("회원+관리자"),
+    ADMIN("관리자");
+
+    private final String title;
+
+    BoardAuthority(String title) {
+        this.title = title;
+    }
+
+    public static List<String[]> getList() {
+        return Arrays.asList(
+          new String[] { ALL.name(), ALL.title },
+          new String[] { MEMBER.name(), MEMBER.title},
+          new String[] { ADMIN.name(), ADMIN.title }
+        );
+    }
+
+    public String getTitle() {
+        return title;
+    }
 }
