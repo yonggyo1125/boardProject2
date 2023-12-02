199355e61c4f1c0e45f3a247417fda016e679449
yonggyo1125 <yonggyo00@kakao.com>
2023-12-02 16:39:11 +0900

    .

--- src/main/java/org/koreait/restcontrollers/files/FileController.java
+++ src/main/java/org/koreait/restcontrollers/files/FileController.java
@@ -7,10 +7,7 @@ import org.koreait.models.file.FileDeleteService;
 import org.koreait.models.file.FileDownloadService;
 import org.koreait.models.file.FileUploadService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
@@ -35,7 +32,11 @@ public class FileController {
      * @return 업로드된 파일 정보를 담은 JSON 응답 데이터
      */
     @PostMapping("/upload")
-    public ResponseEntity<JSONData<List<FileInfo>>> uploadPs(MultipartFile[] files, String gid, String location) {
+    public ResponseEntity<JSONData<List<FileInfo>>> uploadPs(
+            @RequestParam(value = "files", required = false) MultipartFile[] files,
+            @RequestParam(value = "gid", required = false) String gid,
+            @RequestParam(value = "location", required = false) String location) {
+
         List<FileInfo> items = uploadService.upload(files, gid, location);
 
         JSONData<List<FileInfo>> data = new JSONData<>(items);
--- src/main/resources/static/front/js/board/form.js
+++ src/main/resources/static/front/js/board/form.js
@@ -7,3 +7,22 @@ window.addEventListener("DOMContentLoaded", function() {
     })
     .catch(err => console.error(err));
 });
+
+/**
+* 파일 업로드 콜백 처리
+*
+*/
+function fileUploadCallback(files) {
+    if (!files || files.length == 0) {
+        return;
+    }
+
+    for (const file of files) {
+        if (file.location == 'editor') { // 에디터
+            editor.execute('insertImage', { source: file.fileUrl});
+
+        } else { // 첨부 파일
+
+        }
+    }
+}
