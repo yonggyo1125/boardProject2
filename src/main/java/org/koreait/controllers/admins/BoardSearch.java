8df31b9164f9273f53776ab4f1013c91d607b295
yonggyo1125 <yonggyo00@kakao.com>
2023-12-02 12:14:56 +0900

    .

--- src/main/java/org/koreait/controllers/admins/BoardSearch.java
+++ src/main/java/org/koreait/controllers/admins/BoardSearch.java
@@ -2,9 +2,15 @@ package org.koreait.controllers.admins;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class BoardSearch {
     private int page = 1;
     private int limit = 20;
 
+    private String sopt;
+    private String skey;
+    private List<Boolean> active;
+    private List<String> authority;
 }
