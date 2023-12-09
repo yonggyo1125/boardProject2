24f3f0d047174613ee3f8d90a0800b818d2d4548
sachory@naver.com <sachory@naver.com>
2023-12-09 14:11:27 +0900

    .?

--- src/main/java/org/koreait/controllers/boards/BoardForm.java
+++ src/main/java/org/koreait/controllers/boards/BoardForm.java
@@ -20,13 +20,13 @@ public class BoardForm {
 
     private String category;
 
-    @NotBlank(message="제목을 입력하세요.")
+    @NotBlank
     private String subject;
 
-    @NotBlank(message="작성자를 입력하세요.")
+    @NotBlank
     private String poster;
 
-    @NotBlank(message="내용을 입력하세요.")
+    @NotBlank
     private String content;
 
     private boolean notice;
