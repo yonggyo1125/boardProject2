88a7323509157beaa760e3514e104795bb307dae
sachory@naver.com <sachory@naver.com>
2023-12-09 11:15:14 +0900

    .

--- src/main/java/org/koreait/models/board/BoardInfoService.java
+++ src/main/java/org/koreait/models/board/BoardInfoService.java
@@ -1,20 +1,44 @@
 package org.koreait.models.board;
 
 import lombok.RequiredArgsConstructor;
+import org.koreait.commons.ListData;
+import org.koreait.controllers.admins.BoardSearch;
 import org.koreait.entities.BoardData;
+import org.koreait.entities.FileInfo;
+import org.koreait.models.file.FileInfoService;
 import org.koreait.repositories.BoardDataRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class BoardInfoService {
 
     private final BoardDataRepository boardDataRepository;
+    private final FileInfoService fileInfoService;
 
     public BoardData get(Long seq) {
 
         BoardData data = boardDataRepository.findById(seq).orElseThrow(BoardDataNotFoundException::new);
 
+        addFileInfo(data);
+
         return data;
+    }
+
+
+    public ListData<BoardData> getList(BoardSearch search) {
+        
+    }
+
+
+    private void addFileInfo(BoardData data) {
+        String gid = data.getGid();
+        List<FileInfo> editorImages = fileInfoService.getListDone(gid, "editor");
+        List<FileInfo> attachFiles = fileInfoService.getListDone(gid, "attach");
+
+        data.setEditorImages(editorImages);
+        data.setAttachFiles(attachFiles);
     }
 }
