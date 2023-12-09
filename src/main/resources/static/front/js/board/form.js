ce519b67567cf16ac045b7957d758faecfb6390a
sachory@naver.com <sachory@naver.com>
2023-12-09 12:43:39 +0900

    .

--- src/main/resources/static/front/js/board/form.js
+++ src/main/resources/static/front/js/board/form.js
@@ -6,6 +6,13 @@ window.addEventListener("DOMContentLoaded", function() {
         window.editor = editor;
     })
     .catch(err => console.error(err));
+
+
+    /* 이미지 본문 추가 이벤트 처리 */
+    const insertEditors = document.getElementsByClassName("insert_editor");
+    for (const el of insertEditors) {
+        el.addEventListener("click", (e) => insertEditor(e.currentTarget.dataset.url));
+    }
 });
 
 /**
