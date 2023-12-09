9e5ece92a7849a730b31d397c50d5291c7a264a2
sachory@naver.com <sachory@naver.com>
2023-12-09 11:45:43 +0900

    .

--- src/main/java/org/koreait/models/board/BoardInfoService.java
+++ src/main/java/org/koreait/models/board/BoardInfoService.java
@@ -1,15 +1,25 @@
 package org.koreait.models.board;
 
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.dsl.PathBuilder;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.koreait.commons.ListData;
-import org.koreait.controllers.admins.BoardSearch;
+import org.koreait.commons.Utils;
+import org.koreait.controllers.boards.BoardDataSearch;
 import org.koreait.entities.BoardData;
 import org.koreait.entities.FileInfo;
+import org.koreait.entities.QBoardData;
 import org.koreait.models.file.FileInfoService;
 import org.koreait.repositories.BoardDataRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
@@ -17,6 +27,7 @@ public class BoardInfoService {
 
     private final BoardDataRepository boardDataRepository;
     private final FileInfoService fileInfoService;
+    private final EntityManager em;
 
     public BoardData get(Long seq) {
 
@@ -29,7 +40,56 @@ public class BoardInfoService {
 
 
     public ListData<BoardData> getList(BoardDataSearch search) {
+        QBoardData boardData = QBoardData.boardData;
+        int page = Utils.getNumber(search.getPage(), 1);
+        int limit = Utils.getNumber(search.getLimit(), 20);
+        int offset = (page - 1) * limit;
+        
+        String bId = search.getBId(); // 게시판 아이디
+        String sopt  = Objects.requireNonNullElse(search.getSopt(), "subject_content"); // 검색 옵션
+        String skey = search.getSkey(); // 검색 키워드
 
+        BooleanBuilder andBuilder = new BooleanBuilder();
+        andBuilder.and(boardData.board.bId.eq(bId));
+
+        // 키워드 검색 처리
+        if (StringUtils.hasText(skey)) {
+            skey = skey.trim();
+
+            if (skey.equals("subject")) { // 제목 검색
+                andBuilder.and(boardData.subject.contains(skey));
+
+            } else if (skey.equals("content")) { // 내용 검색
+                andBuilder.and(boardData.content.contains(skey));
+
+            } else if (skey.equals("subject_content")) { // 제목 + 내용 검색
+                BooleanBuilder orBuilder = new BooleanBuilder();
+                orBuilder.or(boardData.subject.contains(skey))
+                        .or(boardData.content.contains(skey));
+
+                andBuilder.and(orBuilder);
+            } else if (skey.equals("poster")) { // 작성자 + 아이디
+                BooleanBuilder orBuilder = new BooleanBuilder();
+                orBuilder.or(boardData.poster.contains(skey))
+                        .or(boardData.member.email.contains(skey))
+                        .or(boardData.member.userNm.contains(skey));
+
+                andBuilder.and(orBuilder);
+
+            }
+        }
+
+        PathBuilder pathBuilder = new PathBuilder(BoardData.class, "boardData");
+        List<BoardData> items = new JPAQueryFactory(em)
+                .selectFrom(boardData)
+                .where(andBuilder)
+                .offset(offset)
+                .limit(limit)
+                .fetchJoin()
+                .orderBy(
+                        new OrderSpecifier(Order.valueOf("DESC"),
+                                pathBuilder.get("createdAt")))
+                .fetch();
     }
 
 
