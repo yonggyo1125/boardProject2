a5119b91c6b91a4e59b5b8bcf55be110f1011112
yonggyo1125 <yonggyo00@kakao.com>
2023-12-16 10:54:24 +0900

    .

--- src/main/java/org/koreait/entities/BoardViewId.java
+++ src/main/java/org/koreait/entities/BoardViewId.java
@@ -2,8 +2,10 @@ package org.koreait.entities;
 
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @EqualsAndHashCode
+@NoArgsConstructor
 @AllArgsConstructor
 public class BoardViewId {
     private Long seq;
